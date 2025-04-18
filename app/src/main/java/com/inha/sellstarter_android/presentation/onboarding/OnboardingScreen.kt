d8db852c64b0ab78f641ded52dba568144fc21f2
Seohyeon Shin <ss99x200211@gmail.com>
2025-04-19 00:50:24 +0900

    chore : 불필요 import 삭제

--- app/src/main/java/com/inha/sellstarter_android/presentation/onboarding/OnboardingScreen.kt
+++ app/src/main/java/com/inha/sellstarter_android/presentation/onboarding/OnboardingScreen.kt
@@ -1,19 +1,16 @@
 package com.inha.sellstarter_android.presentation.onboarding
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
