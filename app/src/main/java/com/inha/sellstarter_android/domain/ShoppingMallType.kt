689809ecb05eee91fdfe02dd1824f0114cc2f301
Seohyeon Shin <ss99x200211@gmail.com>
2025-04-19 00:51:06 +0900

    chore : shoppingMallType 패키지 domain 이동에 따른 변경

--- app/src/main/java/com/inha/sellstarter_android/domain/ShoppingMallType.kt
+++ app/src/main/java/com/inha/sellstarter_android/domain/ShoppingMallType.kt
@@ -1,4 +1,4 @@
-package com.inha.sellstarter_android.presentation.onboarding
+package com.inha.sellstarter_android.domain
 
 enum class ShoppingMallType(val displayName: String) {
     GROCERY("식료품점"),
