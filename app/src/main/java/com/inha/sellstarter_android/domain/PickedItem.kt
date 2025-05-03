1fee27f50c346e2ebfa59606ad1e556299074e91
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-03 23:45:49 +0900

    feat : 주문 리스트 뷰 UI 구현

---
+++ app/src/main/java/com/inha/sellstarter_android/domain/PickedItem.kt
@@ -0,0 +1,6 @@
+package com.inha.sellstarter_android.domain
+
+data class PickedItem(
+    val productName: String,
+    val barcode: String
+)
---
+++ app/src/main/java/com/inha/sellstarter_android/presentation/order/confim/component/OrderList.kt
@@ -0,0 +1,75 @@
+package com.inha.sellstarter_android.presentation.order.confim.component
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.Checkbox
+import androidx.compose.material.Divider
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import com.inha.sellstarter_android.domain.Order
+import com.inha.sellstarter_android.ui.theme.Typography
+
+
+@Composable
+fun OrderList(
+    orders: List<Order>,
+    selectedIds: Set<Int>,
+    onItemSelect: (Int) -> Unit
+) {
+    LazyColumn(
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        items(orders) { order ->
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            ) {
+                Checkbox(
+                    checked = selectedIds.contains(order.id),
+                    onCheckedChange = { onItemSelect(order.id) }
+                )
+
+                Spacer(modifier = Modifier.width(12.dp))
+
+                Column {
+                    Row(verticalAlignment = Alignment.CenterVertically) {
+                        Text(
+                            text = order.storeTag,
+                            color = Color.Green,
+                            fontWeight = FontWeight.Bold,
+                            style = Typography.bodySmall
+                        )
+                        Spacer(modifier = Modifier.width(8.dp))
+                        Text(order.productName, style = Typography.bodyMedium)
+                    }
+                    Spacer(modifier = Modifier.height(4.dp))
+                    Text(
+                        text = "옵션: ${order.optionDetail}",
+                        color = Color.Red,
+                        style = Typography.bodySmall
+                    )
+                    Spacer(modifier = Modifier.height(2.dp))
+                    Text(
+                        text = "주문 날짜: ${order.orderDate}",
+                        style = Typography.bodySmall
+                    )
+                }
+            }
+            Divider()
+        }
+    }
+}
