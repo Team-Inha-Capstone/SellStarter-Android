6ef21a19414c98b9ed2dd2cc2a887cbb1d5fd967
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-04 00:27:01 +0900

    fix : 주문 상세확인 뷰와 피킹 아이템 리스트 사이 중첩 스크롤로 인한 오류 수정

--- app/src/main/java/com/inha/sellstarter_android/presentation/order/detail/OrderDetailScreen.kt
+++ app/src/main/java/com/inha/sellstarter_android/presentation/order/detail/OrderDetailScreen.kt
@@ -1,12 +1,18 @@
 package com.inha.sellstarter_android.presentation.order.detail
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -33,59 +39,59 @@ fun OrderDetailScreen(
         PickedItem("미니 드레스 0A92 / 2", "0134Ad234"),
         PickedItem("파란 니트 032Z / 1", "085Ad145")
     )
-
-    Column(
-        modifier = modifier
-            .fillMaxSize()
-    ) {
-        TitleScreen(
-            title = "주문 상세 확인",
-        )
-
-        Column(
-            modifier = Modifier
-                .weight(1f) // 버튼 제외하고 다 차지
-                .padding(bottom = 16.dp) // 버튼과 살짝 거리두기
-                .padding(horizontal = 24.dp)
-        ) {
-
+        Column(modifier = Modifier.fillMaxSize()) {
+            TitleScreen(title = "주문 상세 확인")
             Spacer(modifier = Modifier.height(16.dp))
 
-            OrderInfoContent(
-                modifier = Modifier.fillMaxWidth()
-            )
+            LazyColumn(
+                modifier = Modifier
+                    .weight(1f)
+                    .padding(horizontal = 24.dp),
+                contentPadding = PaddingValues(bottom = 20.dp),
+                verticalArrangement = Arrangement.spacedBy(24.dp)
+            ) {
+                item {
+                    OrderInfoContent(
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
 
-            Spacer(modifier = Modifier.height(24.dp))
+                item {
+                    PickingInfoContent(
+                        pickedItems = pickedItems,
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .heightIn(max = 300.dp)
+                    )
+                }
 
-            PickingInfoContent(
-                pickedItems = pickedItems,
+                item {
+                    BuyerInfoContent(
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+
+                item {
+                    BuyerInfoContent(
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                }
+            }
+
+            OneButton(
+                text = "전체 피킹 완료",
+                buttonBackgroundColor = Blue200,
+                onClick = { },
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(250.dp)
+                    .padding(24.dp),
+                enabled = pickedItems.isNotEmpty()
             )
-
-            Spacer(modifier = Modifier.height(24.dp))
-
-            BuyerInfoContent(
-                modifier = Modifier.fillMaxWidth()
-            )
-
-            Spacer(modifier = Modifier.height(16.dp))
         }
-
-        OneButton(
-            text = "전체 피킹 완료",
-            buttonBackgroundColor = Blue200,
-            onClick = { },
-            modifier = Modifier
-                .padding(24.dp)
-                .fillMaxWidth(),
-            enabled = pickedItems.isNotEmpty() // 조건에 따라 활성화
-        )
-    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(showBackground = true, apiLevel = 33)
 @Composable
 fun PreviewOrderDetailScreen() {
     OrderDetailScreen(
