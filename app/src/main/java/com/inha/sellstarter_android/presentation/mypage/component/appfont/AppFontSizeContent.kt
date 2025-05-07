c66cb5d271da7cb7732424d5c8c66ba2f6de2c82
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-07 16:10:40 +0900

    feat : font 조절 dialog 구현

--- app/src/main/java/com/inha/sellstarter_android/presentation/mypage/component/appfont/AppFontSizeContent.kt
+++ app/src/main/java/com/inha/sellstarter_android/presentation/mypage/component/appfont/AppFontSizeContent.kt
@@ -1,18 +1,20 @@
-package com.inha.sellstarter_android.presentation.mypage.component
+package com.inha.sellstarter_android.presentation.mypage.component.appfont
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import com.inha.sellstarter_android.presentation.common.component.TitleAndText
+import com.inha.sellstarter_android.presentation.mypage.FontScaleViewModel
 
 @Composable
 fun AppFontSizeContent(
     onClickEdit: () -> Unit,
+    fontScale: FontSizeType,
     modifier: Modifier
 ) {
 
     TitleAndText(
         titleText = "앱 글자 크기 조정",
-        contentText = "현재 크기 : 보통",
+        contentText = "현재 크기 : ${fontScale.label}",
         isAvailableEdit = true,
         onClickEdit = onClickEdit,
         modifier = modifier
