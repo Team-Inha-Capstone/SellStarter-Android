a6a240d015144785987d1d858ff5ee872d9fbc09
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-12 23:01:52 +0900

    refactor : loadingScreen 기본값 설정

--- app/src/main/java/com/inha/sellstarter_android/presentation/common/screen/LoadingScreen.kt
+++ app/src/main/java/com/inha/sellstarter_android/presentation/common/screen/LoadingScreen.kt
@@ -15,11 +15,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.inha.sellstarter_android.ui.theme.Grey100
 
 @Composable
 fun LoadingScreen(
     loadingText: String,
-    loadingTextColor: Color,
+    loadingTextColor: Color = Grey100,
     modifier: Modifier
 ) {
     Column(
