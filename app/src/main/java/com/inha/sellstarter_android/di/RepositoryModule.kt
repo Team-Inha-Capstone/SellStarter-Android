8ed3bf8c243e63caaeb063c6fa4e217d79c1e2b3
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-12 23:06:12 +0900

    feat : di module 세팅

--- app/src/main/java/com/inha/sellstarter_android/di/RepositoryModule.kt
+++ app/src/main/java/com/inha/sellstarter_android/di/RepositoryModule.kt
@@ -1,10 +1,17 @@
 package com.inha.sellstarter_android.di
 
+import com.inha.sellstarter_android.data.repository.MyPageRepositoryImpl
+import com.inha.sellstarter_android.domain.repository.MyPageRepository
+import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RepositoryModule {
+    @Binds
+    @Singleton
+    abstract fun bindMyPageRepository(myPageRepositoryImpl: MyPageRepositoryImpl): MyPageRepository
 }
