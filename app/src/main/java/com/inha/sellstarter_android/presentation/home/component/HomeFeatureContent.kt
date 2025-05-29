a5bed3e8982f2aadd60ec4560bece5dd0eae0121
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-29 15:16:27 +0900

    feat : 홈 API 연결 및 Chatbot, 재고등록 연결

--- app/src/main/java/com/inha/sellstarter_android/presentation/home/component/HomeFeatureContent.kt
+++ app/src/main/java/com/inha/sellstarter_android/presentation/home/component/HomeFeatureContent.kt
@@ -9,6 +9,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Card
+import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -17,10 +18,12 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import com.inha.sellstarter_android.ui.theme.AppTypography
+import com.inha.sellstarter_android.ui.theme.Grey0
 
 @Composable
 fun HomeFeatureContent(
-    isDataAnalyticsSubscribed : Boolean,
+    isDataAnalyticsSubscribed: Boolean,
+    onClickInventoryRegister: () -> Unit,
     modifier: Modifier
 ) {
     Row(
@@ -30,6 +33,7 @@ fun HomeFeatureContent(
         HomeFeatureCard(
             title = "재고 등록",
             description = "새롭게 들어온\n물류의 재고를\n등록할 수 있습니다.",
+            onClick = onClickInventoryRegister,
             modifier = Modifier
                 .weight(1f)
                 .height(130.dp)
@@ -38,6 +42,7 @@ fun HomeFeatureContent(
         HomeFeatureCard(
             title = "데이터 분석",
             description = "스토어의 재고와\n판매추이 등을\n분석할 수 있습니다.",
+            onClick = if(isDataAnalyticsSubscribed) onClickInventoryRegister else ({}),
             modifier = Modifier
                 .weight(1f)
                 .height(130.dp)
@@ -45,17 +50,20 @@ fun HomeFeatureContent(
     }
 }
 
+@OptIn(ExperimentalMaterialApi::class)
 @Composable
 fun HomeFeatureCard(
     title: String,
     description: String,
+    onClick: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Card(
         shape = RoundedCornerShape(16.dp),
-        backgroundColor = Color.White, // 원하는 색으로 설정
+        backgroundColor = Grey0,
         modifier = modifier,
-        elevation = 4.dp // 카드의 elevation 설정
+        onClick = onClick,
+        elevation = 4.dp
     ) {
         Column(
             modifier = Modifier.padding(16.dp),
