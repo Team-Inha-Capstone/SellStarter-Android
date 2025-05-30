dbb5c7e944284655e7f46739e74d85890ae8d053
Seohyeon Shin <ss99x200211@gmail.com>
2025-05-30 16:24:08 +0900

    feat : 홈 내 주문 추이 그래프 API 연결

---
+++ app/src/main/java/com/inha/sellstarter_android/data/model/response/home/SalesStatisticsResponseDto.kt
@@ -0,0 +1,30 @@
+package com.inha.sellstarter_android.data.model.response.home
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class WeeklySalesResponseDto(
+    val mon: Int,
+    val tue: Int,
+    val wed: Int,
+    val thu: Int,
+    val fri: Int,
+    val sat: Int,
+    val sun: Int
+)
+
+@Serializable
+data class YearlySalesResponseDto(
+    val jan: Int,
+    val feb: Int,
+    val mar: Int,
+    val apr: Int,
+    val may: Int,
+    val jun: Int,
+    val jul: Int,
+    val aug: Int,
+    val sep: Int,
+    val oct: Int,
+    val nov: Int,
+    val dec: Int
+)
